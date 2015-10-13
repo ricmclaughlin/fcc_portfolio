b0343ff1fa3dad45744ee44e1751486b3ff566e8
ric mclaughlin <ric@mclaughlin.today>
2015-10-13 11:37:28 -0700

    add buildcontrol

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fccPortfolioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fccPortfolioApp
+ */
+angular.module('fccPortfolioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
