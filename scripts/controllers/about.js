5e2ee72c1f0a01ab76b99ed51c7c291fafecc1ab
ric mclaughlin <ric@mclaughlin.today>
2015-11-10 13:53:50 -0800

    gh-pages

---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fccPortfolioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fccPortfolioApp
+ */
+angular.module('fccPortfolioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
